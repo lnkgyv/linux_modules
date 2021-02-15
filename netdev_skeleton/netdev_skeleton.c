571e97b90a8b6f106f8830f8d17bafc12a0744cc
Yuri Goncharuk <lnkgyv@gmail.com>
2021-02-15 18:54:10 +0300

    netdev_skeleton: add multiple ifaces support

--- netdev_skeleton/netdev_skeleton.c
+++ netdev_skeleton/netdev_skeleton.c
@@ -6,7 +6,11 @@
 MODULE_LICENSE( "GPL" );
 MODULE_AUTHOR( "Yuri Goncharuk <lnkgyv@gmail.com>" );
 
-static struct net_device *dev;
+static int dev_num = 0;
+module_param(dev_num, int, S_IRUGO);
+
+static struct net_device *device_array[] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+static int current_device = 0;
 
 static int netdev_skeleton_open(struct net_device *dev)
 {
@@ -42,9 +46,11 @@ static void netdev_setup(struct net_device *dev)
 {
 	int octet_iter;
 
-	for (octet_iter = 0; octet_iter < ETH_ALEN; octet_iter++) {
+	for (octet_iter = 0; octet_iter < ETH_ALEN - 1; octet_iter++) {
 		dev->dev_addr[octet_iter] = (char)octet_iter;
 	}
+
+	dev->dev_addr[ETH_ALEN - 1] = (char)current_device;
 
 	ether_setup(dev);
 	dev->netdev_ops = &ndo;
@@ -52,12 +58,21 @@ static void netdev_setup(struct net_device *dev)
 
 static int __init netdev_skeleton_init(void)
 {
-	dev = alloc_netdev(0, "net_skel%d", 3 /* ifname assigned by userspace */, netdev_setup);
-	if (register_netdev(dev)) {
-		printk(KERN_INFO "Failed to register netdev_skeleton driver\n");
-		free_netdev(dev);
+	if (dev_num <= 0 || dev_num > (sizeof(device_array) / sizeof(device_array[0]))) {
+		printk(KERN_ERR "dev_num less then 0 and greater then %lu\n\
+1..%lu values are allowed\n", sizeof(device_array) / sizeof(device_array[0]),
+			      sizeof(device_array) / sizeof(device_array[0]));
 
 		return -1;
+	}
+
+	for (current_device = 0; current_device < dev_num; current_device++) {
+		device_array[current_device] = alloc_netdev(0, "net_skel%d", 3 /* ifname assigned by userspace */, netdev_setup);
+
+		if (register_netdev(device_array[current_device])) {
+			printk(KERN_INFO "Failed to register netdev_skeleton interface %d\n", current_device);
+			free_netdev(device_array[current_device]);
+		}
 	}
 
 	printk(KERN_INFO "Module 'netdev_skeleton' has loaded!\n");
@@ -67,7 +82,10 @@ static int __init netdev_skeleton_init(void)
  
 static void __exit netdev_skeleton_exit(void)
 {
-	unregister_netdev(dev);
+	for (current_device = 0; current_device < dev_num; current_device++) {
+		unregister_netdev(device_array[current_device]);
+	}
+
 	printk(KERN_INFO "Module 'netdev_skeleton' has unloaded!\n");
 }
 
